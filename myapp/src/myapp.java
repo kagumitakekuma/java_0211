70be3d6e4f0301921bf42bb59987614ad931355f
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-11 16:29:34 +0900

    Add files via upload

---
+++ myapp/src/myapp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class myapp {
+    
+public static void main(String[] args) {
+    System.out.println("Your name?");
+    String name =new Scanner(System.in).next();
+    //String name = "take";
+    
+    System.out.println("hello" +name);
+    System.out.println("hello" +name +"again!");
+
+  }
+
+
+}
