09cda5c143c89b1ba76a60d4b21a831de9a4d4bf
Kagumi Takekuma <80142118+kagumitakekuma@users.noreply.github.com>
2022-02-11 16:44:11 +0900

    Add files via upload

--- myapp/src/myapp.java
+++ myapp/src/myapp.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
-
+import java.util.Random;
 public class myapp {
     
     public static void main(String[] args) { 
-        Integer answer = 6;
+        Integer answer = new Random().nextInt(10) + 1;
 
+        while(true){
     System.out.println("Your guess?");
-    //String name =new Scanner(System.in).nextInt();
-    Integer guess = new Scanner(System.in).nextInt();
-    //String name = "take";
-    if (answer == guess) {
-        System.out.printkn("Bingo!");
+        //String name =new Scanner(System.in).nextInt();
+        Integer guess = new Scanner(System.in).nextInt();
+        //String name = "take";
+        if (answer == guess) {
+            System.out.println("Bingo!");
+            break;
+        }else if(answer > guess){
+    System.out.println("The answer is higher!");
         
+        } else {
+            System.out.println("The answer is lower!");
+        }
+            }
+
+
+        
+        //System.out.println("Your guess:" +guess);
+
+
     }
-    //System.out.println("Your guess:" +guess);
-
-
-  }
 
 
 }
